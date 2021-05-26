88e5fd6f30227a68f67434e5e9b86036c8ea2d11
yangshengaa <yangshengaa@hotmail.com>
2021-05-26 22:26:42 +0800

    finish preprocess and feature engineering; about to test on aws

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -7,6 +7,6 @@ sudo apt-get install python3-venv
 python3 -m venv env
 source env/bin/activate
 pip install "dask[complete]" --no-cache-dir
-pip install dask-ml[xgboost]    # also install xgboost and dask-xgboost
-pip install dask-ml[complete]   # install all optional dependencies
+pip install "dask-ml[xgboost]" --no-cache-dir   # also install xgboost and dask-xgboost
+pip install "dask-ml[complete]" --no-cache-dir  # install all optional dependencies
  
--- feature_prep.py
+++ feature_prep.py
@@ -8,10 +8,14 @@ Author: Jasmine Guan and Sheng Yang
 # testing locally 
 import os 
 import dask.dataframe as dd
+import dask.array as da
 from dask_ml.preprocessing import OneHotEncoder
 
 # constants
 data_path = 'data/historical_data_2009Q1'
+
+# features to use 
+# TODO: do we need to add more?
 feature_cols_idx = [0, 2, 6, 7, 9, 10, 12, 13, 19, 20, 21, 28]
 feature_name = ['CREDIT_SCORE', 
                 'FIRST_TIME_HOMEBUYER_FLAG', 
@@ -35,51 +39,79 @@ def read_origination(file_path):
 
     :param file_path: the path to the origination data
     """
-    df = dd.read_csv(file_path,
+    ddf = dd.read_csv(file_path,
                      sep='|',
                      header=None,
                      usecols=feature_cols_idx,
                      dtype=dict(zip(feature_cols_idx, feature_type))
                     )
-    return df.rename(columns=dict(zip(df.columns, feature_name)))  # change name
+    return ddf.rename(columns=dict(zip(ddf.columns, feature_name)))  # change name
      
 
-def preprocess_origination(df):
+def preprocess_origination(ddf):
     """
-    preprocess
+    preprocess: remove missing entries, scale numeric data, convert booleans,
+    and one hot encode nominal data 
     
-    :param df: the dask dataframe read in
+    :param ddf: the dask dataframe read in
     """
     # drop missing values 
     # TODO: do we need to impute any of these or other columns?
-    df_dropna = df.loc[(df['CREDIT_SCORE'] <= 850) &
-                       (df['CREDIT_SCORE'] >= 301) &
-                       (df['FIRST_TIME_HOMEBUYER_FLAG'] != '9') &
-                       (df['NUMBER_OF_UNITS'] < 5) &
-                       (df['OCCUPANCY_STATUS'] != '9') &
-                       (df['DTI'] != 999) &
-                       (df['CHANNEL'] != '9') & 
-                       (df['LOAN_PURPOSE'] != '9')
+    ddf_dropna = ddf.loc[(ddf['CREDIT_SCORE'] <= 850) &
+                       (ddf['CREDIT_SCORE'] >= 301) &
+                       (ddf['FIRST_TIME_HOMEBUYER_FLAG'] != '9') &
+                       (ddf['NUMBER_OF_UNITS'] < 5) &
+                       (ddf['OCCUPANCY_STATUS'] != '9') &
+                       (ddf['DTI'] != 999) &
+                       (ddf['CHANNEL'] != '9') & 
+                       (ddf['LOAN_PURPOSE'] != '9')
                        ]
     # preprocess numeric and boolean columns 
-    df_num_processed = df_dropna.assign(
-        CREDIT_SCORE=((df['CREDIT_SCORE'] - 301) / (850 - 301)).astype('float16'),    
-        FIRST_TIME_HOMEBUYER_FLAG=df['FIRST_TIME_HOMEBUYER_FLAG'].apply(lambda x: x == 'Y', meta=('int')),
-        HARP_INDICATOR=df['HARP_INDICATOR'].apply(lambda x: x == 'Y', meta=('int'))
+    ddf_num_processed = ddf_dropna.assign(
+        CREDIT_SCORE=((ddf['CREDIT_SCORE'] - 301) / (850 - 301)).astype('float16'),  # don't need many spaces
+        FIRST_TIME_HOMEBUYER_FLAG=ddf['FIRST_TIME_HOMEBUYER_FLAG'].apply(lambda x: x == 'Y', meta=('int')),
+        HARP_INDICATOR=ddf['HARP_INDICATOR'].apply(lambda x: x == 'Y', meta=('int'))
     )
 
     # preprocess nominal columns 
     nom_cols = ['OCCUPANCY_STATUS', 'CHANNEL', 'LOAN_PURPOSE']
     enc = OneHotEncoder(sparse=False, dtype=int)
-    nom_cols_transformed = enc.fit_transform(df_num_processed[nom_cols].categorize())
-    df_proprocessed = dd.concat([df_num_processed, nom_cols_transformed], axis=1, ignore_unknown_divisions=True)
+    nom_cols_transformed = enc.fit_transform(ddf_num_processed[nom_cols].categorize())
+    ddf_proprocessed = dd.concat([ddf_num_processed, nom_cols_transformed], axis=1, ignore_unknown_divisions=True)
 
     # drop original nominal columns 
-    return df_proprocessed.drop(columns=nom_cols)
+    return ddf_proprocessed.drop(columns=nom_cols)
     
 
+def engineer_origination_feature(ddf):
+    """
+    perform feature engineering on the preprocessed dataframe 
+    TODO: add comments on what columns to transform
+
+    :param ddf: the preprocessed dask dataframe 
+    """
+    # log product of interest rate and loan term 
+    log_rate_prod_term = da.log(ddf['ORIGINAL_INTEREST_RATE'] / 100 + 1) * ddf['ORIGINAL_LOAN_TERM']
+
+    # log scaled UPB 
+    log_UPB = da.log(ddf['ORIGINAL_UPB'])
+    
+    return ddf.assign(LOG_RATE_PROD_TERM=log_rate_prod_term.astype('float64'), 
+                      LOG_UPB=log_UPB.astype('float16')
+                    )
+
+
+def write_to_parquet(ddf, ouput_path):
+    """
+    take in the dask dataframe after feature engineering and write to a parquet file 
+
+    :param ddf: the dask dataframe after feature engineering
+    """
+    ddf.to_parquet(output_path)
+    
 
 if __name__ == '__main__':
-    df = read_origination(os.path.join(data_path, 'historical_data_2009Q1.txt'))
-    df = preprocess_origination(df)
-    print(df.compute())
+    ddf = read_origination(os.path.join(data_path, 'historical_data_2009Q1.txt'))
+    ddf_preprocessed = preprocess_origination(ddf)   # preprocess 
+    ddf_engineered = engineer_origination_feature(ddf_preprocessed)  # feature engineering 
+    ddf_engineered.to_parquet('output/')

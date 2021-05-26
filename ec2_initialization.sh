194088f150a06e0467d7fddb34fa57ab38ce2dfb
yangshengaa <yangshengaa@hotmail.com>
2021-05-26 23:33:55 +0800

    part 1 basically finished

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -4,9 +4,10 @@ sudo apt-get update
 # install aws 
 sudo apt-get install awscli
 sudo apt-get install python3-venv
+# install python virtual enviroment 
 python3 -m venv env
 source env/bin/activate
+pip install pyarrow  # for recording parquet
 pip install "dask[complete]" --no-cache-dir
 pip install "dask-ml[xgboost]" --no-cache-dir   # also install xgboost and dask-xgboost
 pip install "dask-ml[complete]" --no-cache-dir  # install all optional dependencies
- 

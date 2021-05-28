a7e3b1cbce6d15aee76732d6d7ee468e023a874d
jasmineguan <feifan.jasmine.guan@gmail.com>
2021-05-27 17:16:27 -0700

    emr initialization file completed

--- emr_initialization.sh
+++ emr_initialization.sh
@@ -5,3 +5,9 @@
     # Application: Spark 3.0.1, Zeppelin 0.9.0
 # Step 2: establish EMR on VSCode
     # ssh -i ~pem_file_location.pem hadoop@ec2-54-190-136-158.us-west-2.compute.
+# Step 3: upload .py file to s3 bucket for faster EMR step
+# Step 4: run file as an EMR step
+    # add step --> step type: Spark Application --> Application Location: choose .py file
+
+
+

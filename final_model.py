f9a400ae51d92dfd02a8fbae5b190b5e76d5f44f
jasmineguan <feifan.jasmine.guan@gmail.com>
2021-05-28 23:15:34 -0700

    final model implemented

--- final_model.py
+++ final_model.py
@@ -6,6 +6,8 @@ Author: Jasmine Guan and Sheng Yang
 """
 
 # load packages
+import dask.dataframe as dd
+
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import FunctionTransformer
@@ -16,15 +18,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 # data import: load parquet file from feature_prep.py & label_prep.py
+feature_ddf = dd.read_parquet("output/feature.parquet/part*").compute()
+label_ddf = dd.read_parquet("output/label.parquet/part*").compute()
+joined_ddf = feature_ddf.merge(label_ddf, on = "LOAN_SEQUENCE_NUMBER", how = "inner").compute()
+X = joined_ddf.drop("label", axis = 1)
+y = joined_ddf[["label"]]
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+# train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
+# logistic regression pipeline
 lor = LogisticRegression(C = 10)
 pl = Pipeline(steps = [('logistic regression', lor)])
 
 # fitting training set on Pipeline
-# pl.fit(X_train, y_train)
+pl.fit(X_train, y_train)
 
 # make prediction & evaluate metric
-# preds = bpl.predict(X_test)
-# f1 = metrics.f1_score(y_test, preds)
+pred = pl.predict(X_test)
+f1 = metrics.f1_score(y_test, pred)
